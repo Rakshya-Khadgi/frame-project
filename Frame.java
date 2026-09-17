1443884cfef076a09db2db722716ac729e21852b
Rakshya <rakshyakhadgi8@gmail.com>
2026-09-17 07:56:05 +0545

    Frist commit of frame

---
+++ Frame.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Frame  {
+    public static void main(String[] args){
+
+        JFrame frame = new JFrame("My Frame");
+
+        frame.setSize(500,400);
+
+        frame.setDefaultCloseOperation(
+            JFrame.EXIT_ON_CLOSE
+        );
+
+        frame.setVisible(true);
+    }
+}
