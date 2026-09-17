36fa1470fac735a8c22db830c2f815f4030bef94
Rakshya <rakshyakhadgi8@gmail.com>
2026-09-17 20:20:50 +0545

    Add feature branch test line

--- Frame.java
+++ Frame.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 
 public class Frame  {
     public static void main(String[] args){
-
+System.out.println("This change is from feature branch");
         JFrame frame = new JFrame("My Frame");
 
         frame.setSize(500,400);
